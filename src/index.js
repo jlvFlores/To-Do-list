a875d4cded6a5871690aeed0eda977d4b51378e3
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-02-21 22:37:47 -0600

    Add dynamic objects array

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-import _ from 'lodash';
 import './style.css';
+
+const listContainer = document.querySelector('ul');
+const listArray = [
+  {
+    description: 'Useless text',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Useless text',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Useless text',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Useless text',
+    completed: false,
+    index: 3,
+  },
+];
+
+// LOAD
+listArray.forEach((element) => {
+  listContainer.insertAdjacentHTML('beforeend', `
+  <li data-li="${element.index}">
+    <input class="checkbox" type="checkbox">
+    <textarea maxlength="255">${element.description}</textarea>
+    <i class="move fa-solid fa-ellipsis-vertical"></i>
+    <i class="erase fa-solid fa-trash" data-id="${element.index}"></i>
+  </li>
+  `);
+});
