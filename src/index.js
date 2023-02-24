cc145d5cfc3faa119cd244e060e3b75cddda3bff
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-02-23 19:28:08 -0600

    OPtimized code and Fix ESlinter errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import './style.css';
 import * as addRemove from './modules/add-remove-list.js';
-import { updateStatus } from './modules/status.js';
+// import * as interactive from './modules/interactive.js';
 
 addRemove.renderList();
 
 // EVENT LISTENERS
 const addBtn = document.getElementById('add-button');
 addBtn.addEventListener('click', () => {
-  const inputValue = document.getElementById('add-input');
-  if (inputValue.value) {
-    addRemove.addToList(inputValue.value);
-    inputValue.value = '';
-    inputValue.focus();
+  const input = document.getElementById('add-input');
+  if (input.value) {
+    addRemove.addToList(input.value);
+    input.value = '';
+    input.focus();
   }
-});
-
-const checkboxes = document.querySelectorAll('.checkbox');
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', () => updateStatus(checkbox));
-});
-
-const descTextareas = document.querySelectorAll('.desc');
-descTextareas.forEach((textarea) => {
-  textarea.addEventListener('keyup', (e) => addRemove.updateDescription(e.target.dataset.ta, textarea.value));
-});
-
-const removeBtns = document.querySelectorAll('.erase');
-removeBtns.forEach((button) => {
-  button.addEventListener('click', (e) => addRemove.removeItem(e.target.dataset.id));
 });
 
 const clearBtn = document.querySelector('.clear-completed');
--- src/modules/status.js
+++
@@ -1,7 +0,0 @@
-import { updateLocalStorage, listArray } from './add-remove-list.js';
-
-export const updateStatus = (checkbox) => {
-  const id = parseInt(checkbox.parentElement.id - 1);
-  checkbox.checked ? listArray[id].completed = true : listArray[id].completed = false;
-  updateLocalStorage();
-};
