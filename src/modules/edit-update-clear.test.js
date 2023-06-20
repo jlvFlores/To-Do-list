0ea6464b9c01a544a69ebb4265e644f27a0a7cbd
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-20 16:14:21 -0600

    Update test files

--- src/modules/edit-update-clear.test.js
+++ src/modules/edit-update-clear.test.js
@@ -2,28 +2,17 @@
  * @jest-environment jsdom
  */
 
-import { renameListItem, clearSelected } from './add-remove-list.js';
-import { updateCompletedV2 } from './update.js';
+import { pushListItem, renameListItem, clearSelected } from './add-remove-list.js';
 
 describe('Testing Crud functions', () => {
+  const chore = 'laundry';
+  const newChore = 'Mop';
+  pushListItem(chore);
+
   test('edit description test', () => {
-    const array = [{ description: 'desc', completed: false, indexVal: 1 }];
-    const changed = [{ description: 'new text', completed: false, indexVal: 1 }];
-    expect(renameListItem(1, 'new text', array)).toStrictEqual(changed);
-  });
-  test('Update task status', () => {
-    const array = [{ description: 'desc', completed: false, indexVal: 1 }];
-    const changed = [{ description: 'desc', completed: true, indexVal: 1 }];
-    expect(updateCompletedV2(0, array)).toStrictEqual(changed);
+    expect(renameListItem(1, newChore)).toStrictEqual([{ description: newChore, completed: false, indexVal: 1 }]);
   });
   test('Clear completed', () => {
-    const array = [
-      { description: 'desc', completed: false, indexVal: 1 },
-      { description: 'desc', completed: true, indexVal: 2 },
-    ];
-    const changed = [
-      { description: 'desc', completed: false, indexVal: 1 },
-    ];
-    expect(clearSelected(array)).toStrictEqual(changed);
+    expect(clearSelected()).toStrictEqual([{ description: newChore, completed: false, indexVal: 1 }]);
   });
 });
